b4ac9eafdf79ff796172c5d5f9a663f8da8cfcd8
habilithm97 <habinkim97@gmail.com>
2023-03-20 23:48:52 +0900

    메인 UI, 아이템 UI

--- app/src/main/java/com/example/simpletodolist/MainActivity.java
+++ app/src/main/java/com/example/simpletodolist/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.simpletodolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class); // ViewModel 객체 생성
+        // List에 포함된 객체들을 관찰함
+        mainViewModel.getAllItems().observe(this, new Observer<List<MainModel>>() {
+            @Override
+            public void onChanged(List<MainModel> mainModels) { // List의 DataSet이 변경될 경우 호출됨
+                //
+            }
+        });
     }
 }
